6c6e75f5666b2f46b450ee1de539c23be0d4e256
Niamul islam neon <106792349+niamulislamneon@users.noreply.github.com>
2022-06-17 09:11:01 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,40 @@
 <!DOCTYPE html>
-<html>
-<title></title>
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<html lang="en">
 <head>
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
+   <h1>Hello world from HTML</h1> 
+   <?php
+        $author = 'faisal';
+   ?>
 
-<?php
-$str ="hello world from mist";
+   <h1>Hello world from 
+        <?php echo $author; ?>
+    </h1>
 
-echo strlen($str).'<br/>';
-echo str_word_count($str).'<br/>
+    <?php
+        // string basics
+        $str = "Hello world from MIST";
 
-?> 
- 
+        echo strlen($str) . '<br />';
+        
+        echo str_word_count($str) . '<br />';
+
+        // arrays
+        $students = [
+            'faisal' => 22,
+            'aziz' => 23,
+            'tamim' => 24
+        ];
+
+        foreach($students as $name=>$roll)
+        {
+            echo "name : " . $name . ' ,' . 'Roll: ' . $roll . '<br />';
+        }
+    ?>
 </body>
 </html>
-
-
